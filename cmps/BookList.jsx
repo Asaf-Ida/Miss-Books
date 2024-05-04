50e8e124f6d8713b582f8c4e0d5e1af4e9c8149a
Asaf-Ida <Asafida20@gmail.com>
2024-05-04 22:49:17 +0300

    Add book details cmp

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,11 +1,13 @@
+import { BookDetails } from "./BookDetails.jsx"
 import { BookPreview } from "./BookPreview.jsx"
 
-export function BookList({ books }) {
+export function BookList({ books, onShowDetails }) {
     return <section className="book-list">
         <ul>
             {books.map(book => 
             <li key={book.id}>
                 <BookPreview book={book} />
+                <button onClick={() => onShowDetails(book)}>Details</button>
             </li>)}
         </ul>
     </section>    
